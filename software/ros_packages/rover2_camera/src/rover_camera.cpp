d5c5e21b1983366d9f81e4922e36032a5c0a93d2
Blake Hakkila <hakkilab@oregonstate.edu>
2023-02-25 20:09:02 +0000

    camera cpp upgrade basic package structure

---
+++ software/ros_packages/rover2_camera/src/rover_camera.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "rover2_camera_interface/msg/camera_control_message.hpp"
+
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+
+  printf("hello world rover2_camera package\n");
+  return 0;
+}
